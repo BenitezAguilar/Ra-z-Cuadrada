6bf50fd60362e71161a83559aef5f526d6fe662c
BEAR <rigobertobeniteztours@gmail.com>
2026-02-28 12:15:22 -0800

    Menú de opciones

--- Raíz.py
+++ Raíz.py
@@ -15,3 +15,16 @@ def calcular_raices(a, b, c):
         x2=(-b-r)/(2*a)
         print("\nEl valor de x1 es: ", x1)
         print("El valor de x2 es: ", x2)
+
+while True:
+    print("--Calculadora de Raíces Cuadradas--")
+    print("1. Calcular raíces")
+    print("2. Salir")
+    opcion = input("\nSeleccione una opción: ")
+    if opcion == "1":
+        calcular_raices(0, 0, 0)
+    elif opcion == "2":
+        print("¡Hasta luego!")
+        break
+    else:
+        print("Opción no válida. Por favor, seleccione una opción válida.")
